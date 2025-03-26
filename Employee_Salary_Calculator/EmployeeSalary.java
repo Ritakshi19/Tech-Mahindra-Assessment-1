408ddf117e295f55d1d592a0191ef60922d52446
Ritakshi Singh <109134641+Ritakshi19@users.noreply.github.com>
2025-03-26 16:56:21 +0530

    Add files via upload

--- Employee_Salary_Calculator/EmployeeSalary.java
+++ Employee_Salary_Calculator/EmployeeSalary.java
@@ -1,1 +1,44 @@
+import java.util.*;
+class Employee
+{
+    private String name;
+    private int ID;
+    private double basic_salary;
+    private double bonus;
+    private double tax;
 
+    Employee(String name, int ID, double basic_salary,double bonus,double tax)
+    {
+        this.name=name;
+        this.ID=ID;
+        this.basic_salary=basic_salary;
+        this.bonus=bonus;
+        this.tax=tax;
+    }
+    
+    public void netSalary()
+    {
+        double net_salary= (basic_salary + bonus) - tax;
+        System.out.println("Net Salary : "+net_salary);
+    }   
+}
+
+public class EmployeeSalary{
+    public static void main(String args[])
+    {
+        Scanner scr=new Scanner(System.in);
+        System.out.println("Enter following details- Name, Employee ID, Basic Salary, Bonus, Tax");
+        String name=scr.nextLine();
+        int ID=scr.nextInt();
+        double basic_salary=scr.nextDouble();
+        double bonus=scr.nextDouble();
+        double tax=scr.nextDouble();
+
+        System.out.println("Employee details are  \nName : "+name+"\nEmployee ID :  "+ID+"\nBasic Salary : "+basic_salary+"\nBonus : "+bonus+"\nTax : "+tax);
+
+        Employee emp=new Employee (name,ID,basic_salary,bonus,tax);
+        emp.netSalary();
+        scr.close();
+
+    }
+}
