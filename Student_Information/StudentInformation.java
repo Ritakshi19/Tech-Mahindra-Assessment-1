8866c4e2a59b17d0be19b8ce1b0089030768b484
Ritakshi Singh <109134641+Ritakshi19@users.noreply.github.com>
2025-03-26 16:57:18 +0530

    Add files via upload

--- Student_Information/StudentInformation.java
+++ Student_Information/StudentInformation.java
@@ -1,1 +1,33 @@
+import java.util.*;
+class Student{
+    private String name;
+    private int roll_no;
+    private double marks;
 
+    Student(String name, int roll_no, double marks){
+        this.name=name;
+        this.roll_no=roll_no;
+        this.marks=marks;
+    }
+
+    public void displayDetails(){
+        System.out.println("Name : "+name+"\nRoll_No : "+roll_no+"\nMarks  : "+marks);
+    }
+}
+
+public class StudentInformation{
+    public static void main(String args[])
+    {
+        Scanner scr=new Scanner(System.in);
+        System.out.println("Enter following details- Name, Roll_No, Marks");
+        String name=scr.nextLine();
+        int roll_no=scr.nextInt();
+        double marks=scr.nextDouble();
+
+        Student student=new Student(name,roll_no,marks);
+        System.out.println("Details of the Student");
+        student.displayDetails();
+        scr.close();
+    
+    } 
+}
